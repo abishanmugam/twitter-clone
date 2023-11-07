e43dd15bcffadd8ece33c49a520554f0bc168053
Mukesh Selvaraj <abirami@ishamistygreen.com>
2023-11-07 19:46:07 +0530

    comit changes in components

---
+++ src/components/Feed.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import './Feed.css';
+import FeedPost from './FeedPost';
+import Post from './Post';
+import db from '../config/firebase';
+
+function Feed() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    db.collection('posts').onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
+    );
+  });
+
+  return (
+    <div className="main border-left border-right">
+      <div className="feed bg-light">
+        <div className="title font-weight-bold p-3 border-bottom sticky-top bg-white">
+          Home
+        </div>
+        <Post />
+        <div className="feed-roll border-top">
+          {posts.map((post) => (
+            <FeedPost
+              name={post.username}
+              username={post.username}
+              avatar={post.avatar}
+              postMsg={post.postMsg}
+              upload={post.upload}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
---
+++ src/components/FeedPost.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Feed.css';
+
+function FeedPost({ name, username, avatar, postMsg, upload }) {
+  return (
+    <div className="feed-post p-3 border-bottom">
+      <div className="media">
+        <img className="img mr-3 rounded-circle" src={avatar} />
+        <div className="media-body">
+          <div className="post-title d-flex flex-column flex-sm-row mb-1">
+            <div class="post-name font-weight-bold mr-2">{name}</div>
+            <div class="post-username text-secondary">@JohnUberbacher · 4h</div>
+          </div>
+          <div class="mb-3">{postMsg}</div>
+          <img src={upload} className="w-100 img-fluid rounded" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FeedPost;
