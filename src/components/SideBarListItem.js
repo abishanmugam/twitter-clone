a62ade50632d1bd870f4c31c9871bdc71b98faea
Mukesh Selvaraj <abirami@ishamistygreen.com>
2023-11-07 19:47:36 +0530

    changes in components

---
+++ src/components/SideBarListItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SideBarListItem.css';
+
+function SideBarListItem({ Text, Icon }) {
+  return (
+    <a href="#" className="sideBarListItem mb-2">
+      <div class="sideBarHover">
+        <div className="sidebar-icon">
+          <Icon />
+        </div>
+        <span class="pr-2">{Text}</span>
+      </div>
+    </a>
+  );
+}
+
+export default SideBarListItem;
